9eac5969f45021fd9e109ae540da0b8baa5c70e2
fengzi2016 <1820166258@qq.com>
2017-11-06 13:25:34 +0800

    'product-page-ok'

--- src/components/product-page.vue
+++ src/components/product-page.vue
@@ -6,7 +6,7 @@
         </div>
             <center-words></center-words>
             <div class="diamond">
-                <div class="diamond_inner"></div>
+                <div class="diamond-inner"></div>
             </div>
             <div class="center-introduce">
                 产品的引领者 团队的粘合剂<br>
@@ -27,16 +27,33 @@
             <div class="center_sprite">
                 <div class="center_sprite_inner"></div>
             </div>
-            <div class="bottom-word">
-            <div class="bottom-word1">发挥你的灵感<br>发挥你的灵感<br>发挥你的灵感</div>
-            <div class="bottom-word2">发挥你的灵感<br>发挥你的灵感<br></div>
-            <div class="bottom-word3">发挥你的灵感</div>
-            <div class="bottom-word4">发挥你的灵感</div>
-            <div class="bottom-word5">发挥你的灵感</div>
-            <div class="bottom-word6">发挥你的灵感<br>发挥你的灵感<br>发挥你的灵感</div>
-            <div class="bottom-word7">发挥你的灵感</div>
-            <div class="bottom-word8">发挥你的灵感</div>
-            <div class="bottom-word9">发挥你的灵感</div>
+            
+            <div class="bottom-word1">
+                <div class="bottom-word1-inner"></div>
+            </div>
+            <div class="bottom-word2">
+                <div class="bottom-word2-inner"></div>
+            </div>
+            <div class="bottom-word3">
+                <div class="bottom-word3-inner"></div>
+            </div>
+            <div class="bottom-word4">
+                <div class="bottom-word4-inner"></div>
+            </div>
+            <div class="bottom-word5">
+                <div class="bottom-word5-inner"></div>
+            </div>
+            <div class="bottom-word6">
+                <div class="bottom-word6-inner"></div>
+            </div>
+            <div class="bottom-word7">
+               <div class="bottom-word7-inner"></div>
+            </div>
+            <div class="bottom-word8">
+                <div class="bottom-word8-inner"></div>
+            </div>
+            <div class="bottom-word9">
+               <div class="bottom-word9-inner">  </div>
             </div>
         </div>
 </template>
@@ -72,13 +89,58 @@ export default {
     left: ($left/750*100);
 }
 
-@mixin pos($top, $left) {
-    position: absolute;
+@mixin rootpos($top, $left) {
+    position: fixed;
     top: ($top/1334*100);
     left: ($left/750*100);
 }
+@mixin animfirst($function){
+  animation: $function 2s ease infinite;
+  -webkit-animation:  $function 2s ease infinite;
+  -o-animation:  $function 2s ease infinite;
+  -moz-animation:  $function 2s ease infinite;
+}
+@mixin animsecond($function){
+  animation: $function 2s ease-in 0.5s infinite;
+  -webkit-animation:  $function 2s ease-in 0.5s infinite;
+  -o-animation:  $function 2s ease-in  0.5s infinite;
+  -moz-animation:  $function 2s ease-in 0.5s infinite;
+}
+@keyframes out {
+   from{
+      opacity: 1;
+   }
+   to{
+       opacity: 0;
+    }
+}
+@-webkit-keyframes out {
+    from{
+        opacity: 1;
+   }
+   to{
+       opacity: 0;
+    }
+}
+@keyframes hidd {
+   from{
+      opacity: 0;
+   }
+   to{
+       opacity: 1;
+    }
+}
+@-webkit-keyframes hidd {
+   from{
+      opacity: 0;
+   }
+   to{
+       opacity: 1;
+    }
+}
 
 .product-page-all {
+    -webkit-text-size-adjust: none;
     width: 100%;
     min-height: 100%;
     background-attachment: fixed;
@@ -97,7 +159,7 @@ export default {
   color: rgb(255, 255, 255);
 }
 .center-word{
-    margin-top: (40vh/1334*100);
+    margin-top: (15vh/1334*100)!important;
 }
 .center-introduce{
     margin-top: (50vh/1334*100);
@@ -108,6 +170,12 @@ export default {
 }
 i{
     margin-left: -3.2em;
+}
+.diamond{
+    @include father(98vw,21vh ,400vh ,330vw );
+}
+.diamond-inner{
+     @include sprite($product-center-diamond,$product-center-diamond-height );
 }
 .center-introduce-right-insert{
 @include father(85vw,168vh ,528vh ,470vw );
@@ -122,23 +190,88 @@ border-color: rgb(255, 255, 255);
 padding-top: 0.6em ;
 }
 .center_sprite{
-    @include father(662vw,505vh,720vh,86vw )
+    @include father(662vw,505vh,745vh,105vw )
 }
 .center_sprite_inner{
     @include sprite($product-center-sprite,$product-center-sprite-height);
 }
-.bottom-word{
-    color:rgb(253, 175, 39);
-    writing-mode: vertical-rl;
+
+.bottom-word1{
+    @include father(11vw,218vh ,960vh ,89vw);
+    @include animfirst(out);
+
+}
+.bottom-word1-inner{
+    @include sprite($product-bottom-word-large,$product-bottom-word-large-height);
+    height: 70%;
+}
+.bottom-word2{
+  
+    @include father(11vw,218vh ,998vh ,137vw );
+    opacity: 0;
+    @include animsecond(hidd);
+}
+.bottom-word2-inner{
+    @include sprite($product-bottom-word-large,$product-bottom-word-large-height);
+}
+.bottom-word3{
+     @include animfirst(out);
+    @include father(22vw,152vh ,1005vh ,210vw );
+}
+.bottom-word3-inner{
+    @include sprite($product-bottom-word-short,$product-bottom-word-short-height);
+}
+.bottom-word4{
+    opacity: 0;
+    @include father(22vw,152vh ,1057vh ,423vw );
+     @include animsecond(hidd);
+}
+.bottom-word4-inner{
+    @include sprite($product-bottom-word-short,$product-bottom-word-short-height);
+    height: 40%;
+}
+.bottom-word5{
+     @include animfirst(out);
+     @include father(22vw,152vh ,1063vh ,445vw );
+}
+.bottom-word5-inner{
+    @include sprite($product-bottom-word-short,$product-bottom-word-short-height);
+}
+.bottom-word6{
+    opacity: 0;
+    @include father(11vw,218vh ,1069vh ,481vw );
+     @include animsecond(hidd);
+}
+.bottom-word6-inner{
+    @include sprite($product-bottom-word-large,$product-bottom-word-large-height);
+    height: 50%;
+}
+.bottom-word7{
+     @include animfirst(out);
+     @include father(22vw,152vh ,1069vh ,521vw );
+    
+}
+.bottom-word7-inner{
+     @include sprite($product-bottom-word-short,$product-bottom-word-short-height);
 }
 
-
-
-
-
-
-
-
+.bottom-word8{
+     opacity: 0;
+     @include father(11vw,218vh ,1055vh ,675vw );
+      @include animsecond(hidd);
+}
+.bottom-word8-inner{
+      @include sprite($product-bottom-word-large,$product-bottom-word-large-height);
+      height: 70%;
+}
+.bottom-word9{
+     @include father(11vw,218vh ,1048vh ,692vw );
+}
+.bottom-word9-inner{
+     @include animfirst(out);
+      @include sprite($product-bottom-word-large,$product-bottom-word-large-height);
+      height: 60%;
+}
 
 
 </style>
